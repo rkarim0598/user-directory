33fea8fafad9225ea7093179b167ada606a55867
Rayyan Karim <rkarim0598@gmail.com>
2018-05-14 18:26:00 -0400

    Change text of first h1 tag on button click

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const button = document.querySelector('button.greeting')
 
-function changeText(ev) {
+function changeText() {
     //const muhParagraph = document.querySelector('p.greeting')
     //muhParagraph.textContent = "I must not fear"
-    ev.target.textContent = 'Clickity click'
+    const muhHeader = document.querySelector('#header1')
+    muhHeader.textContent = "He who controls the spice, controls the Universe"
 }
 
 button.addEventListener('click', changeText)
 
-
+// Homework
+// MAKE BUTTON CHANGE TEXT OF HEADING (THE <H1>) -- check!
+// ADD MULTIPLE HEADINGS TO THE PAGE AND MAKE THE BUTTON CHANGE THE SECOND ONE
+// SUPER MEGA BONUS: ADD A FORM TO THE PAGE
+// ADD A TEXT INPUT TO THE FORM
+// UPDATE HEADING WITH THE TEXT THAT YOU TYPE IN THE TEXT INPUT WHEN FORM IS SUBMITTED
+// 
