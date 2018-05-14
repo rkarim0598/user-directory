5c1a9a5619c5cf382963b8994233b93cb9ee269f
Rayyan Karim <rkarim0598@gmail.com>
2018-05-14 18:31:58 -0400

    Add multiple headings, change the text of the second h1 on button click

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const button = document.querySelector('button.greeting')
 function changeText() {
     //const muhParagraph = document.querySelector('p.greeting')
     //muhParagraph.textContent = "I must not fear"
-    const muhHeader = document.querySelector('#header1')
+    const muhHeader = document.querySelector('#header2')
     muhHeader.textContent = "He who controls the spice, controls the Universe"
 }
 
@@ -11,7 +11,7 @@ button.addEventListener('click', changeText)
 
 // Homework
 // MAKE BUTTON CHANGE TEXT OF HEADING (THE <H1>) -- check!
-// ADD MULTIPLE HEADINGS TO THE PAGE AND MAKE THE BUTTON CHANGE THE SECOND ONE
+// ADD MULTIPLE HEADINGS TO THE PAGE AND MAKE THE BUTTON CHANGE THE SECOND ONE -- check!
 // SUPER MEGA BONUS: ADD A FORM TO THE PAGE
 // ADD A TEXT INPUT TO THE FORM
 // UPDATE HEADING WITH THE TEXT THAT YOU TYPE IN THE TEXT INPUT WHEN FORM IS SUBMITTED
