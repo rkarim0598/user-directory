422f9fcc62621f0076e07c5d9e1fb9938a6ac7d5
Rayyan Karim <rkarim0598@gmail.com>
2018-05-14 16:33:24 -0400

    Change button text when clicked

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const button = document.querySelector('button.greeting')
 
-function changeText() {
-    const muhParagraph = document.querySelector('p.greeting')
-    muhParagraph.textContent = "I must not fear"
+function changeText(ev) {
+    //const muhParagraph = document.querySelector('p.greeting')
+    //muhParagraph.textContent = "I must not fear"
+    ev.target.textContent = 'Clickity click'
 }
 
 button.addEventListener('click', changeText)
