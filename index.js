d4e3f85f1d61c61fd670c473dfba2086d191a8d9
Rayyan Karim <rkarim0598@gmail.com>
2018-05-15 15:45:31 -0400

    Display users in their favorite colors

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const handleSubmit = function(ev) {
   const f = ev.target
   const userName = f.userName.value
   const age = f.age.value
+  const color = f.favoriteColor.value
 //   const age = f.age.value
 //   users.innerHTML += `<p>${userName}, ${age}</p>`
 
   // da complicated way that is better
   const p = document.createElement('p')
   p.textContent = `${userName}, ${age}`
-
+  p.style.backgroundColor = color
   users.appendChild(p)
   f.reset()
   f.userName.focus()
+//   document.body.style.backgroundColor = color;
+//   document.color = color;
+//   document.bgColor = color;
+
 }
 
 form.addEventListener('submit', handleSubmit)
