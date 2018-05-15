7776462b3f88900aae4a871be433d8f25ccb2b6d
Rayyan Karim <rkarim0598@gmail.com>
2018-05-15 00:29:00 -0400

    Form now changes header w/out use of button!

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const button = document.querySelector('button.greeting')
 
 function changeText() {
-    //const muhParagraph = document.querySelector('p.greeting')
-    //muhParagraph.textContent = "I must not fear"
     const muhHeader = document.querySelector('#header2')
+  
     const muhtext = document.querySelector('input')
-    const texty = muhtext.value
-    console.log(texty)
-    muhHeader.textContent = texty
+    muhHeader.textContent = muhtext.value
 }
 
-button.addEventListener('click', changeText)
+document.getElementById('formie').addEventListener("submit", function(event) {
+    event.preventDefault()
+    changeText()
+})
 
 // Homework
 // MAKE BUTTON CHANGE TEXT OF HEADING (THE <H1>) -- check!
